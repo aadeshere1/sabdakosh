007b8af3279a733932db309171a93e0b8bb33cc2
Aadesh <aadeshere1@gmail.com>
2017-04-29 20:56:49 +0545

    import from csv

--- app/models/acronym.rb
+++ app/models/acronym.rb
@@ -3,7 +3,7 @@ class Acronym < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       course_hash = row.to_hash
-      course = find_or_create_by!(acry: course_hash['acry'], full_form: course_hash['full_form'], definition: course_hash['definition'], )
+      course = find_or_create_by!(acry: course_hash['acry'], full_form: course_hash['full_form'])
       course.update_attributes(course_hash)
     end
   end
