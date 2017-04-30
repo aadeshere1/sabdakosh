ff39becb1e528c35600583a9b76f96c200fbe8c1
Aadesh <aadeshere1@gmail.com>
2017-04-30 15:09:33 +0545

    nitty pick

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
   def index
     @rand = Acronym.order("RANDOM()").first
   end
+
+  def about
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :acronyms do
     collection { post :import }
   end
+
+get '/instructions', to: 'static_pages#instruction', as: 'instructions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
